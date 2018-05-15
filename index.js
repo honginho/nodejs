7bde7fc46f23b50e2058ce783e2fd10fddd59e0c
Unknown <sm870831@gmail.com>
2018-05-15 19:56:42 +0800

    create a simple web

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const Xray = require('x-ray')
-const x = Xray()
-x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html', 'table > tr', [
-    {
-        No: 'td:nth-child(2)',
-        courseName: 'td:nth-child(4)',
-        teacher: 'td:nth-child(5)'
-    }
-]).write('./dist/course.json')
+const Express = require('express');
+
+const app = Express();
+
+app.get('/', function (req, res) {
+    console.log('Hello');
+    res.send('Hello World');
+});
+
+app.listen(3000, function () {
+    console.log('start listen http://localhost:3000');
+});
